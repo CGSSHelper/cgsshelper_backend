82176c7ccd58a1442b8cd8f90b5f00d052a63767
DNARoma <tangpei506@gmail.com>
2016-10-09 21:48:57 +0200

    convert png to webp when browser supports

--- handler.py
+++ handler.py
@@ -1,7 +1,27 @@
 from handlers.MainHandler import MainHandler
 from handlers.EventHandler import EventAllHandler, EventDetailHandler, EventNowHandler
 from handlers.CardHandler import CardAllHandler, CharaAllHandler
-import tornado, os
+import tornado
+import os
+import io
+from PIL import Image
+from tornado.web import StaticFileHandler
+
+class myFileHandler(StaticFileHandler):
+
+    def validate_absolute_path(self, root, absolute_path):
+        abspath = super().validate_absolute_path(root, absolute_path)
+        if ("png" in abspath) and ("image/webp" in self.request.headers.get("accept")) and (os.stat(abspath).st_size > 500000):
+            # return png as webp to reduce size
+            try:
+                os.stat(abspath.replace("png", "webp"))
+            except:
+                im = Image.open(abspath).convert("RGB")
+                im.save(abspath.replace("png", "webp"), "WEBP", quality=90)
+            return abspath.replace("png", "webp")
+        else:
+            return abspath
+
 
 endpoints = [
     (r"/", MainHandler),
@@ -10,5 +30,5 @@ endpoints = [
     (r"/event/now", EventNowHandler),
     (r"/card/all", CardAllHandler),
     (r"/chara/all", CharaAllHandler),
-    (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/dest/')}),
+    (r'/static/(.*)', myFileHandler, {'path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/dest/')}),
 ]
