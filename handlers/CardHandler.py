5c147d0742f92a009cde5d18a703c2501dcee4fc
DNARoma <tangpei506@gmail.com>
2016-10-23 13:29:17 +0200

    better gacha

--- handlers/CardHandler.py
+++ handlers/CardHandler.py
@@ -1,7 +1,8 @@
 import os
 import csv
 import random
-import functools
+import heapq
+import math
 import tornado.escape
 from . import BaseHandler
 
@@ -10,65 +11,89 @@ class CardAllHandler(BaseHandler):
 
     def get(self):
         with open(os.path.dirname(__file__) + os.getenv("STATIC_DIR", "/../static/") +
-              "dest/master/card_data.csv", "r", encoding='utf-8') as f:
+                  "dest/master/card_data.csv", "r", encoding='utf-8') as f:
             reader = csv.DictReader(f)
             cards = list(reader)
 
         for card in cards:
-            card["avatar_url"] = "/static/card/card_{0}_m/card_{0}_m.png".format(card["id"])
+            card["avatar_url"] = "/static/card/card_{0}_s/card_{0}_s.png".format(card["id"])
             card["full_url"] = "/static/card/card_{0}_xl/card_{0}_xl.png".format(card["id"])
         self.write(cards)
+
 
 class CardDetailHandler(BaseHandler):
 
     def get(self, card_id):
         with open(os.path.dirname(__file__) + os.getenv("STATIC_DIR", "/../static/") +
-              "dest/master/card_data.csv", "r", encoding='utf-8') as f:
+                  "dest/master/card_data.csv", "r", encoding='utf-8') as f:
             reader = csv.DictReader(f)
             card = list(filter(lambda row: row["id"] == card_id, reader))[0]
-        card["avatar_url"] = "/static/card/card_{0}_m/card_{0}_m.png".format(card["id"])
+        card["avatar_url"] = "/static/card/card_{0}_s/card_{0}_s.png".format(card["id"])
         card["full_url"] = "/static/card/card_{0}_xl/card_{0}_xl.png".format(card["id"])
 
         self.write(card)
 
+
 class CardGachaHandler(BaseHandler):
 
     def post(self):
+        '''
+        params example:
+        {
+            "amount": 10,
+            "type": 1,
+            "probability": [88.5, 10, 1.5],
+            "must": true
+        }
+        '''
         params = tornado.escape.json_decode(self.request.body)
-        total_chance = functools.reduce(lambda x, y: x + y, params["chance"])
-        if total_chance is not 100 or len(params["chance"]) is not 3:
+        total_probability = math.ceil(sum(params["probability"]))
+        if total_probability != 100 or len(params["probability"]) is not 3:
             self.set_status(500)
             self.write({"code": 1, "msg": "Invalid argument"})
             return
 
         with open(os.path.dirname(__file__) + os.getenv("STATIC_DIR", "/../static/") +
-              "dest/master/card_data.csv", "r", encoding='utf-8') as f:
+                  "dest/master/card_data.csv", "r", encoding='utf-8') as f:
             reader = csv.DictReader(f)
-            cards = list(card for card in list(reader) if (int(card["id"]) % 2))
+            cards = list({"id": card["id"], "rarity": card["rarity"]} for card in list(
+                reader) if (int(card["id"]) % 2))
 
         # generate random cards
-        # TODO: at least one SR(rarity 5)
+        # credit: http://huangwei.pro/2015-07/game-random/
+        # with in-game probability, the SSR is really super super rare!
         gacha_cards = []
-        start_rarity = [1, 3]
-        for x in range(params["amount"]):
-            randnum = random.randint(0, 100)
-            rarity = start_rarity[params["type"]]
-            for chance in params["chance"]:
-                if randnum <= chance:
-                    break
-                rarity += 2
-                randnum -= chance
-            print(rarity)
-            selection = list(card for card in cards if (int(card["rarity"]) is rarity))
-            gacha_cards.append(random.choice(selection))
+        rarity = [[1, 3, 5], [3, 5, 7]]
+        wtp = [1. * x / sum(params["probability"]) for x in params["probability"]]
+        result = []
+        randoms = []
+        p = []
+        for i, x in enumerate(wtp):
+            randoms.append(random)
+            p.append((randoms[i].normalvariate(1. / x, 1. / x / 3.), i))
+        heapq.heapify(p)
+        is_must_fulfilled = False if params["must"] else True
+        for i in range(params["amount"]):
+            minp, minj = heapq.heappop(p)
+            print((is_must_fulfilled and i == 9) or (is_must_fulfilled or i != 9), is_must_fulfilled, i)
+            sel_rarity = rarity[params["type"]][minj] if (is_must_fulfilled and i == 9) or (is_must_fulfilled or i != 9) else 5
+            minj = minj if (is_must_fulfilled and i == 9) or (is_must_fulfilled or i != 9) else 1
+            is_must_fulfilled = True if sel_rarity > 4 or is_must_fulfilled else False
+            selection = list(card for card in cards if (int(card["rarity"]) == sel_rarity))
+            result.append(minj)
+            gacha_cards.append(random.choice(selection)["id"])
+            heapq.heappush(p, (randoms[minj].normalvariate(
+                1. / wtp[minj], 1. / wtp[minj] / 3.) + minp, minj))
 
+        random.shuffle(gacha_cards)
         self.write(gacha_cards)
+
 
 class CharaAllHandler(BaseHandler):
 
     def get(self):
         with open(os.path.dirname(__file__) + os.getenv("STATIC_DIR", "/../static/") +
-              "dest/master/chara_data.csv", "r", encoding='utf-8') as f:
+                  "dest/master/chara_data.csv", "r", encoding='utf-8') as f:
             reader = csv.DictReader(f)
             charas = list(reader)
 
--- handlers/__init__.py
+++ handlers/__init__.py
@@ -9,3 +9,10 @@ class BaseHandler(RequestHandler):
         self.add_header("Api-Version", os.getenv("API_VER", "1.0.0"))
         self.add_header("Access-Control-Allow-Origin", "*")
         super().write(payload)
+
+    def options(self):
+        self.add_header("Access-Control-Allow-Origin", "*")
+        self.add_header("Access-Control-Allow-Headers", "content-type")
+        self.add_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        self.set_status(204)
+        self.finish()
